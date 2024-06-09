e8ec12f0c29c3b7a3180734caac5aa8f6397e5dc
catvatar <olekgogol@protonmail.com>
2024-06-09 15:11:38 +0200

    changing directory in setting now creates the new directory

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Obsidian NSFW Plugin
 Obsidian plugin adding a sidebar button that toggles the visibility of NSFW marked files. 
->Currently only supports notes.
 ## Working principle
-Any note that is inside `.folder/` is hidden from view inside obsidian. Pressing the **Toggle Visibility Button** moves marked files from their current location to selected `.folder/`. Plugin automatically creates cash file to save each files old location.
->`.folder` defaults to `.NSFW/`
+Any note that is inside **NSFW folder** is hidden from view inside obsidian. Pressing the **Toggle Visibility Button** moves marked files from their current location to selected **NSFW folder**. Plugin automatically creates cash file to save each files old location.
+>**NSFW folder** defaults to `.NSFW/`
 ## Motivation
 This project was motivated by the need to hide personal notes from view when showing notes in public.
 # Guide
-> TODO
+By either clicking the sidebar eye icon, or the `Toggle NSFW filter` command your files will be moved to a designated **NSFW folder** and excluded from your vault. Doing the same again will move files back.
+# Known Issues
+- When files are moved to the **NSFW folder** their original location is saved to `data.json` in **Plugin Directory**, this may be a concent when using outside tools for vault synchronization
+- Changing **NSFW folder** will create many empty folders inside the **Plugin Directory**
+- Before changing **NSFW folder** make sure to have your vault set to **NSFW**
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Vault, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, TFolder, normalizePath, getFrontMatterInfo, WorkspaceLeaf } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting, TFile, WorkspaceLeaf } from 'obsidian';
 
 const hideStyle = "position: fixed; top: 0; left: -9999px; visibility: hidden;";
 
@@ -115,56 +115,14 @@ export default class ObsidianNSFW extends Plugin {
 		this.NSFWstatus = this.addStatusBarItem();
 		this.NSFWstatus.setText(this.settings.visibility?'NSFW':'SFW');
 
-		// // This adds a simple command that can be triggered anywhere
-		// this.addCommand({
-		// 	id: 'open-sample-modal-simple',
-		// 	name: 'Open sample modal (simple)',
-		// 	callback: () => {
-		// 		new SampleModal(this.app).open();
-		// 	}
-		// });
+		this.addCommand({
+			id: 'toggle NSFW filter',
+			name: 'Toggle NSFW filter',
+			callback: () => {
+				this.toggleVisibility();
+			}
+		})
 
-		// // This adds an editor command that can perform some operation on the current editor instance
-		// this.addCommand({internalPlugins
-		// 	id: 'sample-editor-command', 
-		// 	name: 'Sample editor command',
-		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
-		// 		console.log(editor.getSelection());
-		// 		editor.replaceSelection('Sample Editor Command');
-		// 	}
-		// });
-
-		// // This adds a complex command that can check whether the current state of the app allows execution of the command
-		// this.addCommand({
-		// 	id: 'open-sample-modal-complex',
-		// 	name: 'Open sample modal (complex)',
-		// 	checkCallback: (checking: boolean) => {
-		// 		// Conditions to check
-		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 		if (markdownView) {
-		// 			// If checking is true, we're simply "checking" if the command can be run.
-		// 			// If checking is false, then we want to actually perform the operation.
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-
-		// 			// This command will only show up in Command Palette when the check function returns true
-		// 			return true;
-		// 		}
-		// 	}
-		// });
-
-		
-		// // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// // Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
-			
-		// // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-		
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 	}
 
@@ -224,21 +182,6 @@ export default class ObsidianNSFW extends Plugin {
 
 }
 
-// class SampleModal extends Modal {
-// 	constructor(app: App) {
-// 		super(app);
-// 	}
-
-// 	onOpen() {
-// 		const {contentEl} = this;
-// 		contentEl.setText('Woah!');
-// 	}
-
-// 	onClose() {
-// 		const {contentEl} = this;
-// 		contentEl.empty();
-// 	}
-// }
 
 class SampleSettingTab extends PluginSettingTab {
 	plugin: ObsidianNSFW;
@@ -265,20 +208,27 @@ class SampleSettingTab extends PluginSettingTab {
 			})
 		
 		containerEl.createEl("h2",{ text: 'Configuration' });
+		containerEl.createEl("p",{text:'WARNING: Make sure to set vault to NSFW before changing Ignored directory.'})
 		new Setting(containerEl)
 			.setName('Ignored directory')
-			.setDesc('By default obsidian ignores `.folders/`')
+			.setDesc('By default obsidian ignores folders starting with \'.\'. This has unintended consequence and there is no way to safely remove old Ignored directories from plugin folder, keep that in mind.')
 			.addText(text => {text
 				.setPlaceholder('This should not be empty')
 				.setValue(this.plugin.settings.isolation)
 				.onChange(async (value) => {
+					if(!this.plugin.settings.visibility){
+						new Notice('Set vault to NSFW before changing');
+						return;
+					}
 					this.plugin.settings.isolation = value;
+					this.app.vault.createFolder(this.plugin.settings.isolation);
+					
 					await this.plugin.saveSettings();
 				})
 			})
 		new Setting(containerEl)
 			.setName('Query for NSFW files')
-			.setDesc('Search query for files you want to hide')
+			.setDesc('Search query (\'global-search\') for files you want to hide')
 			.addText(text => {text
 				.setPlaceholder('This should not be empty')
 				.setValue(this.plugin.settings.query)
@@ -287,6 +237,8 @@ class SampleSettingTab extends PluginSettingTab {
 					await this.plugin.saveSettings();
 				})
 			})
+		containerEl.createEl("a",{ text: 'Read More', href: 'https://help.obsidian.md/Plugins/Search' });
+
 		new Setting(containerEl)
 			.setName('Notice on visibility change')
 			.addToggle((cb)=>{cb
