76592f5496fcdced404f921799ac59c7ea416a03
Olek Gogol <olekgogol@gmail.com>
2024-06-10 19:40:23 +0200

    fixed error causing files to not move back to vault on Windows and MacOS

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,17 @@
-import { Notice, Plugin, normalizePath } from 'obsidian';
+import { Notice, Plugin, normalizePath } from "obsidian";
 
-import { obsidianSearchAsync } from './obsidian-search';
+import { obsidianSearchAsync } from "./obsidian-search";
 
-import { DEFAULT_SETTINGS, PluginSettings, SettingsTab } from './settings';
+import { DEFAULT_SETTINGS, PluginSettings, SettingsTab } from "./settings";
 
 export class FileFromTo {
 	fromPath: string;
 	toPath: string;
-	constructor(oldPath:string,newPath:string){
+	constructor(oldPath: string, newPath: string) {
 		this.fromPath = oldPath;
 		this.toPath = newPath;
 	}
 }
-
-
-
 
 export default class ObsidianNSFW extends Plugin {
 	settings: PluginSettings;
@@ -24,41 +21,47 @@ export default class ObsidianNSFW extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		this.app.vault.createFolder(this.settings.isolation).catch(()=>{});
+		this.app.vault.createFolder(this.settings.isolation).catch(() => {});
 
-		if(this.settings.toggleEye){
-			this.toggleVisibilityButton = this.addRibbonIcon('eye', 'Toggle NSFW Visibility', (evt: MouseEvent) => {
-				this.toggleVisibility();
-			});
-			this.toggleVisibilityButton.addClass('my-plugin-ribbon-class');
+		if (this.settings.toggleEye) {
+			this.toggleVisibilityButton = this.addRibbonIcon(
+				"eye",
+				"Toggle NSFW Visibility",
+				(evt: MouseEvent) => {
+					this.toggleVisibility();
+				}
+			);
+			this.toggleVisibilityButton.addClass("my-plugin-ribbon-class");
 		}
 
-
 		this.NSFWstatus = this.addStatusBarItem();
-		this.NSFWstatus.setText(this.settings.visibility?'NSFW':'SFW');
+		this.NSFWstatus.setText(this.settings.visibility ? "NSFW" : "SFW");
 
 		this.addCommand({
-			id: 'toggle NSFW filter',
-			name: 'Toggle NSFW filter',
+			id: "toggle NSFW filter",
+			name: "Toggle NSFW filter",
 			callback: () => {
 				this.toggleVisibility();
-			}
-		})
+			},
+		});
 
 		this.addSettingTab(new SettingsTab(this.app, this));
 	}
 
 	async onunload() {
-		if(!this.settings.visibility){
+		if (!this.settings.visibility) {
 			this.toggleVisibility();
 		}
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
-		
-		
+
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
@@ -70,38 +73,42 @@ export default class ObsidianNSFW extends Plugin {
 		const new_visibility = !this.settings.visibility;
 		this.settings.visibility = new_visibility;
 
-		if(new_visibility){
+		if (new_visibility) {
+			//works on Linux
 			this.settings.whereAreMyFiles.forEach((file) => {
-				const abstractFile = this.app.vault.getAbstractFileByPath(normalizePath(file.toPath));
-				if (abstractFile) {
-					this.app.vault.rename(abstractFile, normalizePath(file.fromPath));
-				}
+				this.app.vault.adapter.rename(
+					normalizePath(file.toPath),
+					normalizePath(file.fromPath)
+				);
 			});
 			this.settings.whereAreMyFiles = [];
-		}else{
+		} else {
+			//works on Linux, Windows and MacOS
 			const searchResult = await obsidianSearchAsync(this.settings.query);
-			searchResult.forEach((_,key)=>{
+			searchResult.forEach((_, key) => {
 				// console.log(key);
-				
+
 				const oldPath = normalizePath(key.path);
-				const newPath = normalizePath(this.settings.isolation + key.name);
-	
-				this.settings.whereAreMyFiles.push(new FileFromTo(
-					oldPath,
-					newPath
-				));
-	
-				this.app.vault.rename(key,newPath)
-			})
+				const newPath = normalizePath(
+					this.settings.isolation + key.name
+				);
+
+				this.settings.whereAreMyFiles.push(
+					new FileFromTo(oldPath, newPath)
+				);
+
+				this.app.vault.rename(key, newPath);
+			});
 		}
 
-		this.NSFWstatus.setText(new_visibility?'NSFW':'SFW');
+		this.NSFWstatus.setText(new_visibility ? "NSFW" : "SFW");
 
-		if(this.settings.toggleNotice){
-			new Notice(new_visibility?'NSFW files visible':'NSFW files NOT visible');
+		if (this.settings.toggleNotice) {
+			new Notice(
+				new_visibility ? "NSFW files visible" : "NSFW files NOT visible"
+			);
 		}
 
 		this.saveSettings();
 	}
-
 }
